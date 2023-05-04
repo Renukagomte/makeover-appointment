4c253241169ceb502af0d38394f83615a3e0e012
renuka gomte <renugomte27@gmail.com>
2023-05-04 19:35:52 +0530

    initial setup

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.static("public"))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://makeover-appointment-production.up.railway.app",
     credentials: true
 }))
 app.use("/employee", require("./routes/employeeRoutes"))
